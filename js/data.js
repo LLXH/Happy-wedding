8a3f49d90bf02d0d2bf6573c7ef44161633bd27f
crazyzhanyue <crazyzhanyue@163.com>
2017-07-23 09:29:56 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,126 @@
+/**
+ * Created by guojingfeng on 2016/12/20.
+ */
+var mockData =[
+  {
+    "fileName": "1.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "2.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "3.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "4.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "5.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "6.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "7.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "8.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "9.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "10.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "11.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "12.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "13.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "14.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "15.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "16.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "17.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "18.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "19.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "20.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "21.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "22.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "23.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+  {
+    "fileName": "24.jpg",
+    "title": "新婚快乐！",
+    "desc": "Happy wedding!"
+  },
+
+]
